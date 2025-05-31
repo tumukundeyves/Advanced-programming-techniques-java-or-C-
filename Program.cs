69a34da8a05cec748f3472d937e0c632dee1607b
tumukundeyves <150735601+tumukundeyves@users.noreply.github.com>
2025-05-31 11:07:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,102 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Delegates_and_Exit_Command
+public delegate void OnResult(string result);
+public delegate string InputProvider();
+
+interface Operation
 {
-    class Program
+    void Perform();
+}
+
+class SumOperation : Operation
+{
+    private InputProvider input;
+    private OnResult onResult;
+
+    public SumOperation(InputProvider input, OnResult onResult)
     {
-        static bool runs = true;
+        this.input = input;
+        this.onResult = onResult;
+    }
 
-        static void Main(string[] args)
+    public void Perform()
+    {
+        int a = int.Parse(input());
+        int b = int.Parse(input());
+        onResult((a + b).ToString());
+    }
+}
+
+class DifOperation : Operation
+{
+    private InputProvider input;
+    private OnResult onResult;
+
+    public DifOperation(InputProvider input, OnResult onResult)
+    {
+        this.input = input;
+        this.onResult = onResult;
+    }
+
+    public void Perform()
+    {
+        int a = int.Parse(input());
+        int b = int.Parse(input());
+        onResult((a - b).ToString());
+    }
+}
+
+class ExitOperation : Operation
+{
+    private Action onExit;
+    private OnResult onResult;
+
+    public ExitOperation(Action exitAction, OnResult resultAction)
+    {
+        this.onExit = exitAction;
+        this.onResult = resultAction;
+    }
+
+    public void Perform()
+    {
+        onExit();
+        onResult("Program end");
+    }
+}
+
+internal class Program
+{
+    static bool runs = true;
+
+    private static void Main(string[] args)
+    {
+        InputProvider input = () => Console.ReadLine();
+        OnResult result = (s) => Console.WriteLine(s);
+
+        var commands = new Dictionary<string, Operation>
         {
-            InputProvider input = () => Console.ReadLine();
-            OnResult result = (s) => Console.WriteLine(s);
+            { "sum", new SumOperation(input, result) },
+            { "dif", new DifOperation(input, result) },
+            { "exit", new ExitOperation(() => runs = false, result) }
+        };
 
-            var commands = new Dictionary<string, Operation>
+        while (runs)
+        {
+            string command = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(command) && commands.ContainsKey(command))
             {
-                { "sum", new SumOperation(input, result) },
-                { "dif", new DifOperation(input, result) },
-                { "exit", new ExitOperation(() => runs = false, result) }
-            };
-
-            while (runs)
-            {
-                Console.Write("Enter command (sum/dif/exit): ");
-                string command = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(command) && commands.ContainsKey(command))
-                {
-                    commands[command].Perform();
-                }
-                else
-                {
-                    Console.WriteLine("Unknown command");
-                }
+                commands[command].Perform();
             }
-        }
-    }
-
-    public delegate string InputProvider();
-    public delegate void OnResult(string result);
-
-    interface Operation
-    {
-        void Perform();
-    }
-
-    class SumOperation : Operation
-    {
-        private InputProvider input;
-        private OnResult onResult;
-
-        public SumOperation(InputProvider input, OnResult onResult)
-        {
-            this.input = input;
-            this.onResult = onResult;
-        }
-
-        public void Perform()
-        {
-            int a = int.Parse(input());
-            int b = int.Parse(input());
-            onResult((a + b).ToString());
-        }
-    }
-
-    class DifOperation : Operation
-    {
-        private InputProvider input;
-        private OnResult onResult;
-
-        public DifOperation(InputProvider input, OnResult onResult)
-        {
-            this.input = input;
-            this.onResult = onResult;
-        }
-
-        public void Perform()
-        {
-            int a = int.Parse(input());
-            int b = int.Parse(input());
-            onResult((a - b).ToString());
-        }
-    }
-
-    class ExitOperation : Operation
-    {
-        private Action onExit;
-        private OnResult onResult;
-
-        public ExitOperation(Action exitAction, OnResult resultAction)
-        {
-            this.onExit = exitAction;
-            this.onResult = resultAction;
-        }
-
-        public void Perform()
-        {
-            onExit();
-            onResult("Program end");
+            else
+            {
+                Console.WriteLine("Unknown command");
+            }
         }
     }
 }
